ed4baf7f4901442941b7c05f624c2febb6aa0942
ChristianNeij <christianneij@gmail.com>
2022-02-10 13:04:42 +0100

    chore: name-attribute renamed.

--- src/com/company/characters/CharacterClass.java
+++ src/com/company/characters/CharacterClass.java
@@ -3,19 +3,19 @@ package com.company.characters;
 import com.company.attributes.PrimaryAttribute;
 
 public abstract class CharacterClass {
-    private final String NAME;
+    private final String name;
     private int level = 1;
     private PrimaryAttribute baseAttributes;
     private PrimaryAttribute totalAttributes;
 
     public CharacterClass(String name, int strength, int dexterity, int intelligence){
-        this.NAME = name;
+        this.name = name;
         this.baseAttributes = new PrimaryAttribute(strength, dexterity, intelligence);
         this.totalAttributes = new PrimaryAttribute(strength, dexterity, intelligence);
     }
 
     public String getName(){
-        return this.NAME;
+        return this.name;
     }
 
     public void incrementLevel(){
