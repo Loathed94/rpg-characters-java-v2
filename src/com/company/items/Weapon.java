16a040b2b5c36dfabcd2a817b2b033709f044599
ChristianNeij <christianneij@gmail.com>
2022-02-10 13:53:00 +0100

    feat: Added damage and attackSpeed as attributes to Weapon class.

--- src/com/company/items/Weapon.java
+++ src/com/company/items/Weapon.java
@@ -2,9 +2,17 @@ package com.company.items;
 
 public class Weapon extends Item{
     private final WeaponType weaponType;
+    private final int damage;
+    private final int attackSpeed;
 
-    public Weapon(String name, int requiredLevel, WeaponType weaponType){
+    public Weapon(String name, int requiredLevel, WeaponType weaponType, int damage, int attackSpeed){
         super(name, requiredLevel, EquipmentSlot.WEAPON);
         this.weaponType = weaponType;
+        this.damage = damage;
+        this.attackSpeed = attackSpeed;
+    }
+
+    public WeaponType getWeaponType() {
+        return this.weaponType;
     }
 }
