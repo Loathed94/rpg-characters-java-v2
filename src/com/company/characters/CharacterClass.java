d6c88b0e6eee561053e04aebe01f377c04d67ade
ChristianNeij <christianneij@gmail.com>
2022-02-10 15:56:34 +0100

    feat: Added 3 new attributes to CharacterClass.

--- src/com/company/characters/CharacterClass.java
+++ src/com/company/characters/CharacterClass.java
@@ -1,12 +1,22 @@
 package com.company.characters;
 
 import com.company.attributes.PrimaryAttribute;
+import com.company.items.ArmorType;
+import com.company.items.EquipmentSlot;
+import com.company.items.Item;
+import com.company.items.WeaponType;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public abstract class CharacterClass {
     private final String name;
     private int level = 1;
     private PrimaryAttribute baseAttributes;
     private PrimaryAttribute totalAttributes;
+    private Map<WeaponType, Boolean> allowedWeaponType = new HashMap<>();
+    private Map<ArmorType, Boolean> allowedArmorType = new HashMap<>();
+    private Map<EquipmentSlot, Item> equipment = new HashMap<>();
 
     public CharacterClass(String name, int strength, int dexterity, int intelligence){
         this.name = name;
