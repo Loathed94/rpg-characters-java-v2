5bd70b001ab09962f9d88531a49b01d2dd631657
ChristianNeij <christianneij@gmail.com>
2022-02-10 13:23:51 +0100

    feat: Created 2 enums, ArmorType and WeaponType.

---
+++ src/com/company/items/ArmorType.java
@@ -0,0 +1,8 @@
+package com.company.items;
+
+public enum ArmorType {
+    CLOTH,
+    LEATHER,
+    MAIL,
+    PLATE
+}
---
+++ src/com/company/items/WeaponType.java
@@ -0,0 +1,11 @@
+package com.company.items;
+
+public enum WeaponType {
+    AXE,
+    BOW,
+    DAGGER,
+    HAMMER,
+    STAFF,
+    SWORD,
+    WAND
+}
