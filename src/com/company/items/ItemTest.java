0ffbd88b2f6a00b65698619a7c9bab571a2eb0c7
ChristianNeij <christianneij@gmail.com>
2022-02-10 13:46:23 +0100

    chore: Created a test case for Item's getters. Only done so using weapon, armor-versions should be added in the future.

---
+++ src/com/company/items/ItemTest.java
@@ -0,0 +1,28 @@
+package com.company.items;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemTest {
+
+    @Test
+    void makeSureGettersWorkAsExpected(){
+        Item weapon1 = new Weapon("Destroyer of Worlds", 100, WeaponType.HAMMER);
+        Item weapon2 = new Weapon("Blunt Dagger of Uselessness", 0, WeaponType.DAGGER);
+        Item weapon3 = new Weapon("A decent wand", 30, WeaponType.WAND);
+
+        Assertions.assertEquals(weapon1.getName(), "Destroyer of Worlds");
+        Assertions.assertEquals(weapon2.getName(), "Blunt Dagger of Uselessness");
+        Assertions.assertEquals(weapon3.getName(), "A decent wand");
+
+        Assertions.assertEquals(weapon1.getRequiredLevel(), 100);
+        Assertions.assertEquals(weapon2.getRequiredLevel(), 0);
+        Assertions.assertEquals(weapon3.getRequiredLevel(), 30);
+
+        Assertions.assertEquals(weapon1.getEquipmentSlot(), EquipmentSlot.WEAPON);
+        Assertions.assertEquals(weapon2.getEquipmentSlot(), EquipmentSlot.WEAPON);
+        Assertions.assertEquals(weapon3.getEquipmentSlot(), EquipmentSlot.WEAPON);
+    }
+}
