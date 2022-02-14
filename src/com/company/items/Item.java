4c982f400b9774b36aa8388d294493fd7aeb41a5
ChristianNeij <christianneij@gmail.com>
2022-02-14 14:44:45 +0100

    docs: Commented the methods of the Item class.

--- src/com/company/items/Item.java
+++ src/com/company/items/Item.java
@@ -5,20 +5,22 @@ public abstract class Item {
     private final int requiredLevel;
     private final EquipmentSlot equipmentSlot;
 
+    //An abstract superclass for different item classes. Stores the shared data and methods for its subclasses.
     public Item(String name, int requiredLevel, EquipmentSlot equipmentSlot){
         this.name = name;
         this.requiredLevel = requiredLevel;
         this.equipmentSlot = equipmentSlot;
     }
 
+    //A simple getter for the name of the item.
     public String getName() {
         return this.name;
     }
 
-    public int getRequiredLevel() {
-        return this.requiredLevel;
-    }
+    //A simple getter for the required level to use item.
+    public int getRequiredLevel() { return this.requiredLevel; }
 
+    //A simple getter for the EquipmentSlot for the item.
     public EquipmentSlot getEquipmentSlot() {
         return this.equipmentSlot;
     }
