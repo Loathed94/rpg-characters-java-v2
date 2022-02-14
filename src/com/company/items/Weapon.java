cdc0f375200290cecb67c1039b3155aaf9329b71
ChristianNeij <christianneij@gmail.com>
2022-02-14 14:44:55 +0100

    docs: Commented the methods of the Weapon class.

--- src/com/company/items/Weapon.java
+++ src/com/company/items/Weapon.java
@@ -5,6 +5,7 @@ public class Weapon extends Item{
     private final double damage;
     private final double attackSpeed;
 
+    //A subclass of Item that represents weapons, it has a WeaponType as well as damage and attack speed.
     public Weapon(String name, int requiredLevel, WeaponType weaponType, double damage, double attackSpeed){
         super(name, requiredLevel, EquipmentSlot.WEAPON);
         this.weaponType = weaponType;
@@ -12,10 +13,12 @@ public class Weapon extends Item{
         this.attackSpeed = attackSpeed;
     }
 
+    //A simple getter for the WeaponType of the weapon object.
     public WeaponType getWeaponType() {
         return this.weaponType;
     }
 
+    //A getter that calculates and returns the DPS-value of the weapon.
     public double getDPS(){
         return this.damage * this.attackSpeed;
     }
