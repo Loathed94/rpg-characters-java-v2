f8f662f17cc820a0ea3c10d81103a5af15e79836
ChristianNeij <christianneij@gmail.com>
2022-02-14 14:08:32 +0100

    docs: Comments in PrimaryAttribute class.

--- src/com/company/attributes/PrimaryAttribute.java
+++ src/com/company/attributes/PrimaryAttribute.java
@@ -5,12 +5,14 @@ public class PrimaryAttribute {
     private int dexterity;
     private int intelligence;
 
+    //A class for storing attribute values for character and Armor objects.
     public PrimaryAttribute(int strength, int dexterity, int intelligence){
         this.strength = strength;
         this.dexterity = dexterity;
         this.intelligence = intelligence;
     }
 
+    //Simple getters for the 3 attributes.
     public int getStrength(){
         return this.strength;
     }
@@ -21,9 +23,8 @@ public class PrimaryAttribute {
 
     public int getIntelligence(){return this.intelligence; }
 
-    public void setStrength(int newStrengthValue){
-        this.strength = newStrengthValue;
-    }
+    //If we wish to set a specific value these 3 methods can be used.
+    public void setStrength(int newStrengthValue){ this.strength = newStrengthValue; }
 
     public void setDexterity(int newDexterityValue){
         this.dexterity = newDexterityValue;
@@ -33,6 +34,7 @@ public class PrimaryAttribute {
         this.intelligence = newIntelligenceValue;
     }
 
+    //If we only wish to increase what is already there these 3 methods can be used.
     public void increaseStrength(int increaseBy){this.strength += increaseBy;}
 
     public void increaseDexterity(int increaseBy){this.dexterity += increaseBy;}
