313925203641b1b5d910e93b555afc86f8c783e1
ChristianNeij <christianneij@gmail.com>
2022-02-14 14:00:07 +0100

    chore: Slight modifications to test cases for better readability and consistency.

--- test/com/company/items/ArmorTest.java
+++ test/com/company/items/ArmorTest.java
@@ -1,57 +1,50 @@
 package com.company.items;
 
-import com.company.items.Armor;
-import com.company.items.ArmorType;
-import com.company.items.EquipmentSlot;
-import com.company.items.Item;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class ArmorTest {
 
     @Test
-    void makeSureAttributesAreCorrect(){
-        Armor armor1 = new Armor("BlaBla", 4, EquipmentSlot.HEAD, ArmorType.LEATHER, 6, 9, 3);
-        Armor armor2 = new Armor("Decent item", 50, EquipmentSlot.BODY, ArmorType.MAIL, 30, 50, 18);
-        Armor armor3 = new Armor("Bretty good leggins", 70, EquipmentSlot.LEGS, ArmorType.CLOTH, 58, 49, 88);
+    void TestGetAttributes_AttributesOfArmorAtInstantiation_ShouldReturnCorrectValues(){
+        Armor armor = new Armor("BlaBla", 4, EquipmentSlot.HEAD, ArmorType.LEATHER, 6, 9, 3);
 
-        Assertions.assertEquals(armor1.getAttributes()[0], 6);
-        Assertions.assertEquals(armor1.getAttributes()[1], 9);
-        Assertions.assertEquals(armor1.getAttributes()[2], 3);
+        int actualStrength = armor.getAttributes()[0];
+        int expectedStrength = 6;
+        int actualDexterity = armor.getAttributes()[1];
+        int expectedDexterity = 9;
+        int actualIntelligence = armor.getAttributes()[2];
+        int expectedIntelligence = 3;
 
-        Assertions.assertEquals(armor2.getAttributes()[0], 30);
-        Assertions.assertEquals(armor2.getAttributes()[1], 50);
-        Assertions.assertEquals(armor2.getAttributes()[2], 18);
-
-        Assertions.assertEquals(armor3.getAttributes()[0], 58);
-        Assertions.assertEquals(armor3.getAttributes()[1], 49);
-        Assertions.assertEquals(armor3.getAttributes()[2], 88);
+        Assertions.assertEquals(actualStrength, expectedStrength);
+        Assertions.assertEquals(actualDexterity, expectedDexterity);
+        Assertions.assertEquals(actualIntelligence, expectedIntelligence);
     }
 
     @Test
-    void makeSureArmorTypesAreCorrect(){
+    void TestGetArmorType_InstantiateArmorObjectWithArmorType_ShouldReturnCorrectArmorType(){
         Armor leather = new Armor("BlaBla", 4, EquipmentSlot.HEAD, ArmorType.LEATHER, 6, 9, 3);
-        Armor mail = new Armor("Decent item", 50, EquipmentSlot.BODY, ArmorType.MAIL, 30, 50, 18);
-        Armor cloth = new Armor("Bretty good leggins", 70, EquipmentSlot.LEGS, ArmorType.CLOTH, 58, 49, 88);
-        Armor plate = new Armor("Breastplate of Awesomeness", 666, EquipmentSlot.BODY, ArmorType.PLATE, 321, 275, 100);
 
-        Assertions.assertEquals(leather.getArmorType(), ArmorType.LEATHER);
-        Assertions.assertEquals(mail.getArmorType(), ArmorType.MAIL);
-        Assertions.assertEquals(cloth.getArmorType(), ArmorType.CLOTH);
-        Assertions.assertEquals(plate.getArmorType(), ArmorType.PLATE);
+        ArmorType actualType = leather.getArmorType();
+        ArmorType expectedType = ArmorType.LEATHER;
+
+        Assertions.assertEquals(actualType, expectedType);
     }
 
     @Test
-    void makeSureSendingWeaponSlotToArmorClassThrowsIAE(){
-        String message = "Armor cannot be assigned to weapon slot!";
+    void TestIllegalArgumentExceptionAtInstantiation_InstantiateArmorWithWeaponSlot_ShouldThrowIAE(){
+        String expectedExceptionMessage = "Armor cannot be assigned to weapon slot!";
+
         Throwable expectedException = Assertions.assertThrows(
                 IllegalArgumentException.class,
                 () -> {
                     Item armor = new Armor("A fitting name", 8, EquipmentSlot.WEAPON, ArmorType.MAIL, 6, 8, 1);
                 }
         );
-        assertEquals(expectedException.getMessage(), message);
+        String actualExceptionMessage = expectedException.getMessage();
+
+        Assertions.assertEquals(actualExceptionMessage, expectedExceptionMessage);
     }
 }
