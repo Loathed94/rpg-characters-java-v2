7fe4adc8f223cf3b261a896f2c92643bfd3ce7c1
ChristianNeij <christianneij@gmail.com>
2022-02-14 09:41:36 +0100

    chore: Added attribute test case on Rogue class for case when rogue is first instantiated. And modified old test case for attributes after levelup.

--- src/com/company/characters/RogueClassTest.java
+++ src/com/company/characters/RogueClassTest.java
@@ -7,32 +7,44 @@ import static org.junit.jupiter.api.Assertions.*;
 class RogueClassTest {
 
     @Test
-    void makeSureAttributesIncreaseAsTheyShouldForMageClass(){
-        CharacterClass rogue = new RogueClass("Rogue");
-
-        assertEquals(rogue.getBaseAttributes().getStrength(), 2);
-        assertEquals(rogue.getBaseAttributes().getDexterity(), 6);
-        assertEquals(rogue.getBaseAttributes().getIntelligence(), 1);
-        assertEquals(rogue.getTotalAttributes().getStrength(), 2);
-        assertEquals(rogue.getTotalAttributes().getDexterity(), 6);
-        assertEquals(rogue.getTotalAttributes().getIntelligence(), 1);
+    void GetAttributes_BaseAndTotalAfterIncrementLevel_ExpectedValues3And10And2(){
+        String characterName = "Akama";
+        CharacterClass rogue = new RogueClass(characterName);
+        String expectedValues = "3102";
+        String actualValuesBase = "";
+        String actualValuesTotal = "";
 
         rogue.incrementLevel();
 
-        assertEquals(rogue.getBaseAttributes().getStrength(), 3);
-        assertEquals(rogue.getBaseAttributes().getDexterity(), 10);
-        assertEquals(rogue.getBaseAttributes().getIntelligence(), 2);
-        assertEquals(rogue.getTotalAttributes().getStrength(), 3);
-        assertEquals(rogue.getTotalAttributes().getDexterity(), 10);
-        assertEquals(rogue.getTotalAttributes().getIntelligence(), 2);
+        actualValuesBase += rogue.getBaseAttributes().getStrength();
+        actualValuesBase += rogue.getBaseAttributes().getDexterity();
+        actualValuesBase += rogue.getBaseAttributes().getIntelligence();
 
-        rogue.incrementLevel();
+        actualValuesTotal += rogue.getTotalAttributes().getStrength();
+        actualValuesTotal += rogue.getTotalAttributes().getDexterity();
+        actualValuesTotal += rogue.getTotalAttributes().getIntelligence();
 
-        assertEquals(rogue.getBaseAttributes().getStrength(), 4);
-        assertEquals(rogue.getBaseAttributes().getDexterity(), 14);
-        assertEquals(rogue.getBaseAttributes().getIntelligence(), 3);
-        assertEquals(rogue.getTotalAttributes().getStrength(), 4);
-        assertEquals(rogue.getTotalAttributes().getDexterity(), 14);
-        assertEquals(rogue.getTotalAttributes().getIntelligence(), 3);
+        assertEquals(actualValuesBase, expectedValues);
+        assertEquals(actualValuesTotal, expectedValues);
+    }
+
+    @Test
+    void GetAttributes_BaseAndTotalAfterInstantiation_ExpectValues2And6And1(){
+        String characterName = "Akama";
+        CharacterClass rogue = new RogueClass(characterName);
+        String expectedValues = "261";
+        String actualValuesBase = "";
+        String actualValuesTotal = "";
+
+        actualValuesBase += rogue.getBaseAttributes().getStrength();
+        actualValuesBase += rogue.getBaseAttributes().getDexterity();
+        actualValuesBase += rogue.getBaseAttributes().getIntelligence();
+
+        actualValuesTotal += rogue.getTotalAttributes().getStrength();
+        actualValuesTotal += rogue.getTotalAttributes().getDexterity();
+        actualValuesTotal += rogue.getTotalAttributes().getIntelligence();
+
+        assertEquals(actualValuesBase, expectedValues);
+        assertEquals(actualValuesTotal, expectedValues);
     }
 }
