b92fbe581d43c01636e65364dfdc8997b9b426b7
ChristianNeij <christianneij@gmail.com>
2022-02-14 13:44:08 +0100

    chore: Slight modifications to test cases for better readability and consistency.

--- test/com/company/items/WeaponTest.java
+++ test/com/company/items/WeaponTest.java
@@ -1,41 +1,29 @@
 package com.company.items;
 
-import com.company.items.Weapon;
-import com.company.items.WeaponType;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class WeaponTest {
 
     @Test
-    void makeSureWeaponTypeFromGetterIsCorrect(){
+    void TestGetWeaponType_WeaponAtInstantiation_ShouldReturnCorrectType(){
         Weapon hammer = new Weapon("Destroyer of Worlds", 100, WeaponType.HAMMER, 600, 2);
-        Weapon dagger = new Weapon("Blunt Dagger of Uselessness", 0, WeaponType.DAGGER, 1, 2);
-        Weapon wand = new Weapon("A decent wand", 30, WeaponType.WAND, 60, 3);
-        Weapon sword = new Weapon("A sharp sword", 46, WeaponType.SWORD, 100, 2);
-        Weapon axe = new Weapon("Axe of Hellscream", 70, WeaponType.AXE, 140, 3);
-        Weapon bow = new Weapon("A bow!", 5, WeaponType.BOW, 13, 1);
-        Weapon staff = new Weapon("A gnarly staff", 29, WeaponType.STAFF, 40, 2);
 
-        Assertions.assertEquals(hammer.getWeaponType(), WeaponType.HAMMER);
-        Assertions.assertEquals(dagger.getWeaponType(), WeaponType.DAGGER);
-        Assertions.assertEquals(wand.getWeaponType(), WeaponType.WAND);
-        Assertions.assertEquals(sword.getWeaponType(), WeaponType.SWORD);
-        Assertions.assertEquals(axe.getWeaponType(), WeaponType.AXE);
-        Assertions.assertEquals(bow.getWeaponType(), WeaponType.BOW);
-        Assertions.assertEquals(staff.getWeaponType(), WeaponType.STAFF);
+        WeaponType expectedType = WeaponType.HAMMER;
+        WeaponType actualType = hammer.getWeaponType();
+
+        Assertions.assertEquals(actualType, expectedType);
     }
 
     @Test
-    void makeSureDPSIsCorrect(){
+    void TestGetDPS_WeaponsDPSAfterInstantiation_ShouldReturnValue1200(){
         Weapon hammer = new Weapon("Destroyer of Worlds", 100, WeaponType.HAMMER, 600, 2);
-        Weapon dagger = new Weapon("Blunt Dagger of Uselessness", 0, WeaponType.DAGGER, 1.5, 2);
-        Weapon wand = new Weapon("A decent wand", 30, WeaponType.WAND, 60.5, 3.75);
 
-        assertEquals(hammer.getDPS(), 1200);
-        assertEquals(dagger.getDPS(), 3.0);
-        assertEquals(wand.getDPS(), 226.875);
+        double expectedDPS = 600*2;
+        double actualDPS = hammer.getDPS();
+
+        Assertions.assertEquals(actualDPS, expectedDPS);
     }
 }
