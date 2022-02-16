484c79bbb952c98bfd4976e452c0e0008471adec
ChristianNeij <christianneij@gmail.com>
2022-02-16 11:04:27 +0100

    chore: Fixing wrong order of variables in assertions in tests and other minor amendments to improve structure. WeaponTest.

--- test/com/company/items/WeaponTest.java
+++ test/com/company/items/WeaponTest.java
@@ -9,21 +9,21 @@ class WeaponTest {
 
     @Test
     void TestGetWeaponType_WeaponAtInstantiation_ShouldReturnCorrectType(){
-        Weapon hammer = new Weapon("Destroyer of Worlds", 100, WeaponType.HAMMER, 600, 2);
-
         WeaponType expectedType = WeaponType.HAMMER;
+        Weapon hammer = new Weapon("Destroyer of Worlds", 100, expectedType, 600, 2);
+
         WeaponType actualType = hammer.getWeaponType();
 
-        Assertions.assertEquals(actualType, expectedType);
+        Assertions.assertEquals(expectedType, actualType);
     }
 
     @Test
     void TestGetDPS_WeaponsDPSAfterInstantiation_ShouldReturnValue1200(){
         Weapon hammer = new Weapon("Destroyer of Worlds", 100, WeaponType.HAMMER, 600, 2);
 
-        double expectedDPS = 600*2;
         double actualDPS = hammer.getDPS();
+        double expectedDPS = 600*2;
 
-        Assertions.assertEquals(actualDPS, expectedDPS);
+        Assertions.assertEquals(expectedDPS, actualDPS);
     }
 }
