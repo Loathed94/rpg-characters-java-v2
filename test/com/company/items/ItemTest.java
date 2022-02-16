ce11770fc4d7c38acca30a79b1ea38557627e614
ChristianNeij <christianneij@gmail.com>
2022-02-16 11:00:32 +0100

    chore: Fixing wrong order of variables in assertions in tests and other minor amendments to improve structure. ItemTest.

--- test/com/company/items/ItemTest.java
+++ test/com/company/items/ItemTest.java
@@ -9,17 +9,17 @@ class ItemTest {
 
     @Test
     void TestItemGetters_GettersAfterInstantiation_ShouldReturnSameValuesAsWasPutInParameter(){
-        Item weapon = new Weapon("Destroyer of Worlds", 100, WeaponType.HAMMER, 600, 2);
+        String expectedName = "Destroyer of Worlds";
+        int expectedRequiredLevel = 100;
+        EquipmentSlot expectedSlot = EquipmentSlot.WEAPON;
+        Item weapon = new Weapon(expectedName, expectedRequiredLevel, WeaponType.HAMMER, 600, 2);
 
         String actualName = weapon.getName();
-        String expectedName = "Destroyer of Worlds";
         int actualRequiredLevel = weapon.getRequiredLevel();
-        int expectedRequiredLevel = 100;
         EquipmentSlot actualSlot = weapon.getEquipmentSlot();
-        EquipmentSlot expectedSlot = EquipmentSlot.WEAPON;
 
-        Assertions.assertEquals(actualName, expectedName);
-        Assertions.assertEquals(actualRequiredLevel, expectedRequiredLevel);
-        Assertions.assertEquals(actualSlot, expectedSlot);
+        Assertions.assertEquals(expectedName, actualName);
+        Assertions.assertEquals(expectedRequiredLevel, actualRequiredLevel);
+        Assertions.assertEquals(expectedSlot, actualSlot);
     }
 }
